537f4df6087240ded6507ba95677b23822e99021
Bronek Szulc <bronek@fastmail.fm>
2025-02-06 09:25:32 -0500

    my attempt (classes)

--- src/gilded-rose.js
+++ src/gilded-rose.js
@@ -1,62 +1,141 @@
 class Item {
+  static minQuality = 0;
+  static maxQuality = 50;
+  static qualityChangeIncrement = 1;
+
   constructor(name, sellIn, quality) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
   }
+
+  updateQuality() {
+    if (this.quality === Item.minQuality) return;
+
+    if (this.sellIn <= 0) {
+      this.quality -= (Item.qualityChangeIncrement * 2);
+    } else {
+      this.quality -= Item.qualityChangeIncrement;
+    }
+  }
+
+  updateSellIn() {
+    if (this.sellIn == null) return;
+
+    this.sellIn -= 1;
+  }
+
+  update() {
+    this.updateQuality();
+    this.updateSellIn();
+  }
+}
+
+class VintageWineItem extends Item {
+  static name = 'Vintage Wine';
+
+  constructor(name, sellIn, quality) {
+    super(name, sellIn, quality);
+  }
+
+  updateQuality() {
+    if (this.quality === Item.maxQuality) return;
+
+    if (this.sellIn <= 0) {
+      this.quality += (Item.qualityChangeIncrement * 2);
+    } else {
+      this.quality += Item.qualityChangeIncrement;
+    }
+  }
+}
+
+class ConcertTicketsItem extends Item {
+  static name = 'Concert Tickets';
+
+  constructor(name, sellIn, quality) {
+    super(name, sellIn, quality);
+  }
+
+  updateQuality() {
+    if (this.quality === Item.maxQuality) return;
+
+    if (this.sellIn < 0) {
+      this.quality = 0;
+    } else if (this.sellIn <= 5) {
+      this.quality += 3;
+    } else if (this.sellIn <= 10 && this.sellIn > 5) {
+      this.quality += 2;
+    } if (this.sellIn > 10) {
+      this.quality += 1;
+    }
+  }
+}
+
+class DiamondItem extends Item {
+  static name = 'Diamond';
+
+  constructor(name, _sellIn, quality) {
+    super(name, null, quality);
+  }
+
+  updateQuality() {
+    return; // no-op
+  }
+
+  updateSellIn() {
+    return; // no-op
+  }
+
+  update() {
+    return; // no-op
+  }
+}
+
+class ChocolateCakeItem extends Item {
+  static name = 'Chocolate Cake';
+  static qualityChangeIncrement = Item.qualityChangeIncrement * 2;
+
+  constructor(name, sellIn, quality) {
+    super(name, sellIn, quality);
+  }
+
+  updateQuality() {
+    if (this.quality === Item.minQuality) return;
+
+    if (this.sellIn <= 0) {
+      this.quality -= (ChocolateCakeItem.qualityChangeIncrement * 2);
+    } else {
+      this.quality -= ChocolateCakeItem.qualityChangeIncrement;
+    }
+  }
+}
+
+function itemFactory({ name, sellIn, quality }) {
+  switch (name) {
+    case VintageWineItem.name:
+      return new VintageWineItem(name, sellIn, quality);
+
+    case ConcertTicketsItem.name:
+      return new ConcertTicketsItem(name, sellIn, quality);
+
+    case DiamondItem.name:
+      return new DiamondItem(name, sellIn, quality);
+
+    case ChocolateCakeItem.name:
+      return new ChocolateCakeItem(name, sellIn, quality);
+
+    default:
+      return new Item(name, sellIn, quality);
+  }
 }
 
 class Shop {
   constructor(items = []) {
-    this.items = items;
+    this.items = items.map(itemFactory);
   }
 
   update() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Vintage Wine' && this.items[i].name != 'Concert Tickets') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Diamond') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Concert Tickets') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Diamond') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Vintage Wine' && this.items[i].name != 'Diamond') {
-          if (this.items[i].name != 'Concert Tickets') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Diamond') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
-    }
+    this.items.forEach((item) => item.update());
 
     return this.items;
   }
--- test/gilded-rose.test.js
+++ test/gilded-rose.test.js
@@ -3,7 +3,7 @@ const { Item, Shop } = require('../src/gilded-rose');
 describe('Gilded Rose', () => {
   const run = (items) => {
     const gildedRose = new Shop(items);
-    
+
     return gildedRose.update();
   };
 
@@ -75,11 +75,55 @@ describe('Gilded Rose', () => {
     });
 
     describe('Concert Tickets', () => {
-      // TODO: I promise this works... not writing tests.
+      it("should decrease the item's sellIn by 1", () => {
+        const items = run([new Item('Concert Tickets', 10, 50)]);
+
+        expect(items[0].sellIn).toBe(9);
+      });
+
+      it("should increase the item's quality by 1 if sellIn > 10", () => {
+        const items = run([new Item('Concert Tickets', 11, 10)]);
+
+        expect(items[0].quality).toBe(11);
+      });
+
+      it("should increase the item's quality by 2 if sellIn <= 10 and > 5", () => {
+        const items = run([new Item('Concert Tickets', 10, 10)]);
+
+        expect(items[0].quality).toBe(12);
+      });
+
+      it("should increase the item's quality by 3 if sellIn <= 5", () => {
+        const items = run([new Item('Concert Tickets', 5, 10)]);
+
+        expect(items[0].quality).toBe(13);
+      });
+
+      it("should set the item's quality to 0 if sellIn < 0", () => {
+        const items = run([new Item('Concert Tickets', -1, 10)]);
+
+        expect(items[0].quality).toBe(0);
+      });
     });
 
     describe('Chocolate Cake', () => {
-      // TODO: Future feature.
+      it("should decrease the item's sellIn by 1", () => {
+        const items = run([new Item('Chocolate Cake', 10, 50)]);
+
+        expect(items[0].sellIn).toBe(9);
+      });
+
+      it("should decrease the item's quality by 2 if sellIn is > 0", () => {
+        const items = run([new Item('Chocolate Cake', 10, 50)]);
+
+        expect(items[0].quality).toBe(48);
+      });
+
+      it("should decrease the item's quality by 4 if sellIn is <= 0", () => {
+        const items = run([new Item('Chocolate Cake', 0, 50)]);
+
+        expect(items[0].quality).toBe(46);
+      });
     });
   });
 });
